f7f3e694cda171fcc95fa2356e3aa2bdcfd200cc
damzylance <wasiuadegoke14@gmail.com>
2023-11-25 04:12:21 +0100

    update ether.js

--- packages/react-app/utils/transaction.ts
+++ packages/react-app/utils/transaction.ts
@@ -1,4 +1,4 @@
-import { ethers, Contract } from "ethers";
+import { ethers, Contract, BrowserProvider, parseEther } from "ethers";
 
 const CUSD_ADDRESS = "0x765de816845861e75a25fca122bb6898b8b1282a";
 
@@ -8,15 +8,12 @@ export const transferCUSD = async (
   amount: string
 ) => {
   if (window.ethereum) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new BrowserProvider(window.ethereum);
     const signer = await provider.getSigner(userAddress);
 
     let abi = ["function transfer (addres to, unit256 value)"];
     let CUSDContract = new Contract(CUSD_ADDRESS, abi, signer);
-    let txn = await CUSDContract.transfer(
-      address,
-      ethers.utils.parseEther(amount)
-    );
+    let txn = await CUSDContract.transfer(address, parseEther(amount));
     let receipt = await txn;
   }
 };
