0c715901389940a0339f7b5aefb265a10631a249
damzylance <wasiuadegoke14@gmail.com>
2023-11-30 05:23:18 +0100

    initialize minipay

--- packages/react-app/components/WidgetContainer.tsx
+++ packages/react-app/components/WidgetContainer.tsx
@@ -1,13 +1,28 @@
 import { HStack, Text, VStack } from "@chakra-ui/react";
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useEffect, useState } from "react";
 import logo from "../public/assets/logo-inline-transparent.png";
 import Image from "next/image";
 import Link from "next/link";
+import { useConnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 interface Props {
   children: ReactNode;
 }
 const WidgetContainer: FC<Props> = ({ children }) => {
+  const [hideConnectBtn, setHideConnectBtn] = useState(false);
+
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+
+  useEffect(() => {
+    if (window.ethereum && window.ethereum.isMiniPay) {
+      setHideConnectBtn(true);
+      connect();
+    }
+  }, []);
+
   return (
     <VStack w={"full"} bg={"#fff"}>
       <HStack w={"full"}>
